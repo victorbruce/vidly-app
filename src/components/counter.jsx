34d13067e0f3b5762a1dee56522b327716e3cde9
Victor Bruce <victorbruce82@gmail.com>
2019-07-09 23:32:08 +0000

    Add project setup

---
+++ src/components/counter.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+
+  getBadgeClass() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+}
+
+export default Counter;
---
+++ src/components/movies.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+class Movies extends Component {
+  state = {
+    movies: null
+  };
+
+  handleDelete = movie => {
+    const movies = [...this.state.movies];
+    const filteredMovies = movies.filter(m => m._id !== movie);
+    this.setState({ movies: filteredMovies });
+  };
+
+  doGetMovies = () => {
+    const movies = getMovies();
+    this.setState({ movies });
+  };
+
+  componentWillMount() {
+    this.doGetMovies();
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    if (movies.length === 0)
+      return <p className="mt-5">There are no movies in the database</p>;
+
+    return (
+      <React.Fragment>
+        <p className="mt-5">Showing {movies.length} movies in the database.</p>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie._id)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;

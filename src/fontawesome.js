84843e1a02ba210342030501537acf4c527d2116
Victor Bruce <victorbruce82@gmail.com>
2019-07-10 19:32:42 +0000

    Revert "Add fontawesome library"
    This reverts commit 08c43f988cb45bea54699e772fa9dbfda96db2c7.

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,13 +1,9 @@
 // import the library
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 // import your icons
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-
-
 
 library.add(
-  faCoffee,
-  faHeart
+  faCoffee
 )

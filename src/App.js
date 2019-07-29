1fee89615f2b1e298c8529038c8fcc82a1ef3fcd
Victor Bruce <victorbruce82@gmail.com>
2019-07-29 13:36:56 +0000

    Add movie form route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/navigation';
-import Movie from './components/movies';
+import Movies from './components/movies';
+import MovieForm from './components/movieForm';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/not-found';
@@ -13,11 +14,12 @@ function App() {
       <Navigation />
       <main className="container">
         <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
           <Route path="/rentals" component={Rentals} />
           <Route path="/customers" component={Customers} />
-          <Route path="/movies" component={Movie} />
           <Route path="/not-found" component={NotFound} />
-          <Route path="/" exact component={Movie} />
+          <Redirect from="/" exact to="/movies" />
           <Redirect to="/not-found" />
         </Switch>
       </main>

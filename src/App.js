a8a94f748168766e6c2b3ad1ac9568d85f87a029
Victor Bruce <victorbruce82@gmail.com>
2019-07-29 12:32:35 +0000

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/navigation';
 import Movie from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/not-found';
 
 function App() {
   return (
-    <main className="container">
-      <Movie />
-    </main>
+    <React.Fragment>
+      <Navigation />
+      <main className="container">
+        <Route path="/rentals" component={Rentals} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/movies" component={Movie} />
+        <Route path="/not-found" component={NotFound} />
+        <Route path="/" exact component={Movie} />
+        <Redirect to="/not-found" />
+      </main>
+    </React.Fragment>
   );
 }
 
